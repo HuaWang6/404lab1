263f26741d2c765fcc6e0438c63c3464117c49f4
hua wang <hua6@ualberta.ca>
2023-01-11 20:20:36 -0500

    Initial commit

--- find_version.py
+++ find_version.py
@@ -2,4 +2,5 @@ import requests
 #print current python version
 print(requests.__version__)
 #print google home page's information
-print(requests.get("http://www.google.com").text)
+print(requests.get("https://raw.githubusercontent.com/HuaWang6/404lab1/master/find_version.py").text)
+
